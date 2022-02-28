29724f3ea55091ebc9b542b478ec3637322b58d6
Tomas Edson <t.edson.almeida@hotmail.com>
2022-02-27 23:27:57 -0300

    Início do progeto

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora PHP</title>
+</head>
+<body>
+
+<form method="post" action="calcular.php">
+    <label for="numero1">Primeiro Número:
+        <input type="text" name="numero1" id="numero1">
+    </label>
+    
+    <label for="numero2">Segundo Número:
+        <input type="text" name="numero2" id="numero2">
+    </label>
+    
+
+</form>
+    
+</body>
+</html>
